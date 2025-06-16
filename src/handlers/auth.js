4a565f8da2d1a4a8803dafab2ac46b68fe939483
d4t06 <huudat01234560@gmail.com>
2025-06-16 22:04:50 +0700

    Updates

--- src/handlers/auth.js
+++ src/handlers/auth.js
@@ -56,7 +56,6 @@ class AuthHandler {
                     httpOnly: true,
                     maxAge: 30 * 24 * 60 * 60 * 1000,
                     secure: process.env.NODE_ENV === "production",
-                    sameSite: "none",
                 });
                 return (0, myResponse_1.default)(res, true, "login successful", 200, {
                     userInfo: {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,6 +8,6 @@ const auth_1 = __importDefault(require("../handlers/auth"));
 const authRouter = (0, express_1.Router)();
 authRouter.post("/login", auth_1.default.login);
 authRouter.post("/register", auth_1.default.register);
-authRouter.get("/refresh", auth_1.default.refreshToken);
+authRouter.post("/refresh", auth_1.default.refreshToken);
 authRouter.get("/logout", auth_1.default.logout);
 exports.default = authRouter;
